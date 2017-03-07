fc06857bbea998ee1274e39f67738b752015a799
DrMeepster <mgonikberg@yahoo.com>
2017-03-06 20:11:36 -0800

    #3 fixed.

--- src/main/java/drmeepster/bable/util/TabBable.java
+++ src/main/java/drmeepster/bable/util/TabBable.java
@@ -2,13 +2,16 @@ package drmeepster.bable.util;
 
 import drmeepster.bable.ModBables;
 import drmeepster.bable.item.BableItems;
+import drmeepster.drcorester.property.PropertyHandler;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 public class TabBable extends CreativeTabs{
-
+	
+	public static final String NAME = "tabBable";
+	
 	public TabBable() {
-		super(ModBables.MODID + ":TabBable");
+		super(PropertyHandler.getName(ModBables.MODID, NAME));
 	}
 
 	@Override
